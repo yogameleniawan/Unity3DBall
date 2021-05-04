96477e22b23a459a5e3eb8f4c1d7bd0002eb50f2
Yoga Meleniawan Pamungkas <yogameleniawan@gmail.com>
2021-05-04 14:25:49 +0700

    commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float ballSpeed;
+ 
+ 	// Update is called once per frame
+	void Update () {
+	float xSpeed = Input.GetAxis("Horizontal");
+	float ySpeed = Input.GetAxis("Vertical");
+	
+	Rigidbody body = GetComponent<Rigidbody> ();
+	body.AddTorque (new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/CameraMotor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour {
+
+	public GameObject target;
+	public float xOffset, yOffset, zOffset;
+	
+	void Update () {
+		transform.position = target.transform.position + new Vector3(xOffset, yOffset, zOffset);
+		transform.LookAt(target.transform.position);
+	}
+}
