09bcd3a53ebf1d1a10c448115882955a99de5b2d
Yoga Meleniawan Pamungkas <yogameleniawan@gmail.com>
2021-06-06 16:52:38 +0700

    checkpoint + finish flag

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject flag;
+    public GameObject flag1;
+    Vector3 spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint= gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y < -20f)
+        {
+            gameObject.transform.position = spawnPoint;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("checkpoint")){
+            spawnPoint = flag.transform.position;
+            Destroy(flag);
+        }else if(other.gameObject.CompareTag("checkpoint1")){
+            spawnPoint = flag1.transform.position;
+            Destroy(flag1);
+        }
+    }
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public GameObject trap;
+    public GameObject trap1;
+    public GameObject trap2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("trap")){
+            Destroy(trap);
+            Destroy(trap1);
+            Destroy(trap2);
+        }
+    }
+}
