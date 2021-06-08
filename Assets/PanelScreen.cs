e435f668bfe180ff3b60fa57eda76d7f0c8be432
hhidayatinc <chasanah55@gmail.com>
2021-06-08 20:59:00 +0700

    panel

---
+++ Assets/PanelScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelScreen : MonoBehaviour
+{
+    public GameObject PanelScrn;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            PanelScrn.gameObject.SetActive(true);
+             Time.timeScale = 0;
+        }
+    }
+
+    private void Awake()
+    {
+        PanelScrn.gameObject.SetActive(false);
+    }
+}
